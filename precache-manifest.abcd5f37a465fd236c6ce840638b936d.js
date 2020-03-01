bc362c525cb7623fcc1e4a18f38a4161bc26b494
Neeraj Shah <neerajcshah@gmail.com>
2020-03-01 14:14:39 -0800

    Updates

---
+++ precache-manifest.abcd5f37a465fd236c6ce840638b936d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6522f791be2f6cc6400d13d05a7f6de0",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "7e44ebb8e1ccb858a56c",
+    "url": "/corona/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c9074e3ae366f844137c",
+    "url": "/corona/static/js/2.c575e876.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corona/static/js/2.c575e876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e44ebb8e1ccb858a56c",
+    "url": "/corona/static/js/main.971cd8e4.chunk.js"
+  },
+  {
+    "revision": "c1d3cc2e7ddf87646fe5",
+    "url": "/corona/static/js/runtime-main.b7b4df54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
