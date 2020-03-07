3a6db747d64f1a27b559f895c17791044f86fcf8
Neeraj Shah <neerajcshah@gmail.com>
2020-03-07 10:24:33 -0800

    two types of mapping

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker
+} from "react-simple-maps";
+
+import { Map, TileLayer } from 'react-leaflet';
+
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Leaflet />
     </div>
   );
+}
+
+const Leaflet = () => {
+  const position = [51.505, -0.09];
+  const zoom = 13;
+  return (
+  <Map center={position} zoom={zoom}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    />
+  </Map>
+  )
+}
+
+const MapChart = () => {
+  return (
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies
+            .map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#EAEAEC"
+                stroke="#D6D6DA"
+              />
+            ))
+        }
+      </Geographies>
+
+</ComposableMap>)
+
 }
 
 export default App;
